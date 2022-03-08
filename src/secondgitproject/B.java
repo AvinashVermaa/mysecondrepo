4179449f2aa1ed2b6bb01be35cbaeec6eeec3819
Avinash Verma <avinashverma657@gmail.com>
2022-03-08 20:13:15 +0530

    second new file

---
+++ src/secondgitproject/B.java
@@ -0,0 +1,8 @@
+package secondgitproject;
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("This is second file in java");
+		System.out.println("Apne Bhagya se kha raha ha");
+	}
+}
