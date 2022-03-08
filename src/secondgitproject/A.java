f1e80305a22457d0fc53f1e5c89cf8947f963b87
Avinash Verma <avinashverma657@gmail.com>
2022-03-08 20:01:49 +0530

    modify

--- src/secondgitproject/A.java
+++ src/secondgitproject/A.java
@@ -3,5 +3,6 @@ package secondgitproject;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("second git repository");
+		System.out.println("This is second line");
 	}
 }
